7cc66b9d00ab264b59b7c17fc182467239abbfa6
Sattvik Chakravarthy <sattvik@gmail.com>
2016-03-03 17:43:55 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pylibs-pyconn',
+      version='1.0',
+      description='Python TCP connection framework.',
+      author='Sattvik Chakravarthy',
+      author_email='sattvik@gmail.com',
+      url='https://github.com/pylibs/pyconn/',
+      packages=['pyconn', 'pyconn.tests'],
+    )
